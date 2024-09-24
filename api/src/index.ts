968408847d9768189ad778a64b80f1c875c3bcef
adaiuto1 <78216021+adaiuto1@users.noreply.github.com>
2024-09-24 13:34:01 -0500

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,16 @@
 import express from "express";
 import { levelRouter } from "./routers/levelRouter";
 import cors from "cors";
-import https from "https";
-import fs from "fs";
+const app = express();
+app.use(express.json());
 const PORT = 8000;
-const PORT_DEV = 8001;
+
 const corsOption = {
 	credentials: true,
 	origin: ["*"],
 };
 
-const app = express();
-
-app.use(express.json());
 app.use(cors(corsOption));
-
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header(
@@ -27,14 +23,6 @@ app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 app.use("/levels", levelRouter);
-https
-	.createServer(
-		{
-			key: fs.readFileSync("localhost-key.pem"),
-			cert: fs.readFileSync("localhost.pem"),
-		},
-		app
-	)
-	.listen(PORT, "0.0.0.0", () => {
-		console.log(`HTTPS server running on port ${PORT}`);
-	});
+app.listen(PORT, () => {
+	console.log(`API is listening at ${PORT}`);
+});
